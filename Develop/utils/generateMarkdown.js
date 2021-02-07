1f71fa1f2de10b1f9dbaaf7b630daccc270e40f8
amatt <amattson84@gmail.com>
2021-02-07 12:03:21 -0800

    wrote functionality for render (LicenseBadeg,LicenseLink,LicenseSection)

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,71 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(licenses) {
+  let licenses = [];
+  let badgeEl = licenses;
+
+  for (let i = 0; i < licenses.length; i++) {
+    if (badgeEl === licenses[i]) {
+      return badgeEl;
+    } else {
+      return "";
+    }
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseL = [];
+  let linkEl = license.link
+
+  for (let i = 0; i < licenseL.length; i++) {
+    if (licenseL === licenseL[i]) {
+      return licenseL;
+    } else {
+      return "";
+    }
+  }
+}
+
+
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseS = [];
+  let sectionEl = license.section;
+
+  for (let i = 0; i < licenseS.length; i++) {
+    if (sectionEl === licenseS[i]) {
+      return sectionEl;
+    } else {
+      return "";
+    }
+  }
+}
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+
+  let licenseBadge = renderLicenseBadge();
+  let licenseLink = renderLicenseLink();
+  let licenseSection = renderLicenseSection();
+  let markDownData = [licenseBadge,licenseLink,licenseSection];
+  
+  value.forEach(markDownData => {
+
+    if (!value === NULL) {
+      data.append(value);
+    }
+        
+
   return `# ${data.title}
 
 `;
-}
+})
 
 module.exports = generateMarkdown;
