2fb922aa6ea097f937f4b56bb08b2b545eb3351d
amatt <amattson84@gmail.com>
2021-02-07 16:02:21 -0800

    wrote user input questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,19 +4,79 @@ let inquirer = require("inquirer");
 let generateMarkdown = require("./utils/generateMarkdown");
 let axios = require("axios");
 
-
-
-
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is you Githibn user name ?",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "What is the name of your repository?",
+    name: "repository",
+  },
+  {
+    type: "input",
+    message: "What is your title ?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What is the project description?",
+    name: "description",
+  },
+
+  {
+    type: "input",
+    message: "What are the installation requirements?",
+    name: "installation",
+  },
+
+  {
+    type: "input",
+    message: "What are the instructions while using this project ?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Please enter the names of any contributors:",
+    name: "contirbutors",
+    default: "Aimee Mattson",
+  },
+  {
+    type: "input",
+    message: "Test examples for this project:",
+    name: "test",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(data => {
+        const queryUrl = ("https://api.github.com/users/" + data.username);
+
+        axios.get(queryUrl).then(response => {
+            const githubUser = {                
+                githubImage: response.data.avatar_url,
+                githubEmail: response.data.email,
+                githubProfile: response.data.html_url,
+                githubName: response.data.name,
+            };
+            fs.writeToFile("README.md", generateMarkdown(data, githubUser), function(err) {
+                if (err) {
+                    throw err;
+                };
+            });
+        }); 
+    });
+    function init() {
+
+    }
+}
 
 // Function call to initialize app
 init();
-
-
